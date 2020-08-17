c6fa125c5d12600b088839d49ce485d42fb302d1
Bruno Shiroma <brunoshiroma@gmail.com>
2020-08-17 18:50:09 -0300

    added build info on about

--- app/src/main/java/com/brunoshiroma/benchtool_android/AboutActivity.kt
+++ app/src/main/java/com/brunoshiroma/benchtool_android/AboutActivity.kt
@@ -7,9 +7,15 @@ import android.text.Html
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.core.text.HtmlCompat.FROM_HTML_MODE_COMPACT
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.google.android.material.appbar.MaterialToolbar
 
 class AboutActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
@@ -24,11 +30,21 @@ class AboutActivity : AppCompatActivity() {
 
         var html = getString(R.string.about_html)
 
+        html = html.replace("\$VERSION", BuildConfig.VERSION_NAME)
+        html = html.replace("\$DATE", BuildConfig.BUILD_YEAR)
+
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             aboutText.text = Html.fromHtml(html, FROM_HTML_MODE_COMPACT)
         } else {
             aboutText.text = Html.fromHtml(html)
         }
         aboutText.movementMethod =LinkMovementMethod.getInstance()
+
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
